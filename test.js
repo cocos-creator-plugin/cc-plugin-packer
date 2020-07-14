862fb302ee8e6ed85a14f15ab841d41436a5fb58
xuyanfeng <xu_yanfeng@126.com>
2020-07-14 19:01:36 +0800

    init

--- test.js
+++ test.js
@@ -5,10 +5,12 @@ packagePlugin({
     plugin: 'test-plugin',
     // 过滤插件的文件，相对于plugin目录的相对路径
     filterFiles: [
-        ''
+        'README.md',
+        'readme.js' // 无效的文件
     ],
     dontMinJs: [
-        ''
+        'panel/index.js',
+        'panel/item.js' // 不存在的文件
     ], // 不压缩的JS代码
     out: '', // 默认的插件打包文件存放位置，默认在project/out
 });
